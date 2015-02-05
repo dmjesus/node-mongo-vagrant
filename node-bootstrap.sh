4d7caf380ef6df8c6f514021b5a5ba4cc603a1f6
Diego Mesquita <diego.mesquita.jesus@gmail.com>
2015-02-05 17:49:02 -0200

    Update mongodb install ubuntu
    Personal config for mongodb

--- node-bootstrap.sh
+++ node-bootstrap.sh
@@ -7,8 +7,8 @@ sudo su
 if [ ! -d "/var/www" ]
 then
     # Add MongoDB to apt
-    apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-    echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+    echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 
     # Update and begin installing some utility tools
     apt-get -y update
@@ -24,7 +24,7 @@ then
     apt-get install -y nodejs
 
     # Install latest stable version of MongoDB
-    apt-get install -y mongodb-10gen
+    apt-get install -y mongodb-org
 
     # Symlink our host www to the guest /var/www folder
     ln -s /vagrant/www /var/www
@@ -32,6 +32,4 @@ then
     # Victory!
     echo "You're all done! Your default node server should now be listening on http://10.0.33.34/. For code, see: node-mongo-vagrant/www/default/server.js."
 
-    # Run it
-    node /var/www/default/server.js
 fi
